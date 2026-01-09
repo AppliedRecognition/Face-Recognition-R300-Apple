cb477e798e2311b09089039d103e6f6f39991423
Jakub Dolejs <jakubdolejs@gmail.com>
2026-01-09 21:56:27 +0000

    Updated face detection dependency

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/AppliedRecognition/Face-Recognition-ArcFace-Apple.git", .upToNextMajor(from: "1.2.2"))
+        .package(url: "https://github.com/AppliedRecognition/Face-Recognition-ArcFace-Apple.git", .upToNextMajor(from: "2.0.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
