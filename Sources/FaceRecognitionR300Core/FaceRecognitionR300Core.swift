5940c53b0dce6f095871e88dfcb679729f163c30
Jakub Dolejs <jakubdolejs@gmail.com>
2026-01-09 21:58:12 +0000

    Pick closest detected faces from candidates when refining faces for template extraction

--- Sources/FaceRecognitionR300Core/FaceRecognitionR300Core.swift
+++ Sources/FaceRecognitionR300Core/FaceRecognitionR300Core.swift
@@ -161,14 +161,21 @@ open class FaceRecognitionR300Core: FaceRecognition {
     }
     
     @_spi(Testing) public func refineFaces(_ faces: [Face], inImage image: Image) async throws -> [Face] {
-        let detectedFaces = try await self.faceDetection.detectFacesInImage(image, limit: faces.count)
-        guard detectedFaces.count == faces.count else {
+        var detectedFaces = try await self.faceDetection.detectFacesInImage(image, limit: faces.count + 10)
+        guard detectedFaces.count >= faces.count else {
             throw FaceRecognitionError.faceDetectionFailure
         }
         let refinedFaces: [Face] = faces.compactMap { originalFace in
-            return detectedFaces.min { a, b in
+            guard let closestFace = detectedFaces.min(by: { a, b in
                 return a.eyeCentre.distance(to: originalFace.eyeCentre) < b.eyeCentre.distance(to: originalFace.eyeCentre)
+            }) else {
+                return nil
             }
+            guard let index = detectedFaces.firstIndex(of: closestFace) else {
+                return nil
+            }
+            detectedFaces.remove(at: index)
+            return closestFace
         }
         guard refinedFaces.count == faces.count else {
             throw FaceRecognitionError.faceDetectionFailure
