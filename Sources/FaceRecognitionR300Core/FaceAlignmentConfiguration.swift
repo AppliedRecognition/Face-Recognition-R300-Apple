0e99ed310697aec645e8f672cc160e67b785281b
Jakub Dolejs <jakubdolejs@gmail.com>
2026-02-03 11:13:06 +0000

    Added face alignment configuration struct

---
+++ Sources/FaceRecognitionR300Core/FaceAlignmentConfiguration.swift
@@ -0,0 +1,14 @@
+//
+//  FaceAlignmentConfiguration.swift
+//  FaceRecognitionR300
+//
+//  Created by Jakub Dolejs on 03/02/2026.
+//
+
+import Foundation
+
+public struct FaceAlignmentConfiguration {
+    public var scale: Double = 2.85
+    public var verticalOffset: Double = 0.4
+    internal init() {}
+}
